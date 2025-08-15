3aff33b0f3191d8259f3a7103785c3b430a3eabe
Charlotte <cnkuna920@gmail.com>
2025-08-15 08:41:05 +0200

    changed dog sound effect

--- index.js
+++ index.js
@@ -7,21 +7,21 @@ class Pet {
       this.name = name;
       this.soundText = soundText;
       this.image = image;
-      this.soundUrl = new Audio(soundUrl) 
-      this.playCount = 0; 
+      this.soundUrl = new Audio(soundUrl) //from the library // its encapsulation: covering sound tyrack/properties  its abstraction: we dont know where it comes from
+      this.playCount = 0; //counter variable
     }
   
     speak() {
-      alert(this.soundText); 
-      this.soundUrl.currentTime = 0; 
+      alert(this.soundText); //alert box
+      this.soundUrl.currentTime = 0; // ensures restart on each click. cant stop/pause
       this.soundUrl.play();
     }
   
     play() {
-      this.playCount++; 
-      this.soundUrl.load; 
-      this.speak(); 
-      updateCounter(this.playCount); 
+      this.playCount++; //increament from 0 to1
+      this.soundUrl.load; //makes sure that the sound can start without delay
+      this.speak(); // we are looping back to speak function, to make the sound keep playing
+      updateCounter(this.playCount); //got mapped with count
       showFunActions(this.name);
     }
   }
@@ -35,7 +35,7 @@ class Pet {
   function choosePet(type) {
     if (type === 'dog') {
       currentPet = new Pet('Dog', 'You have petted the dog! 🐶', 'https://images.pexels.com/photos/2664417/pexels-photo-2664417.jpeg',
-        'media/big-dog-barking-300504.mp3');
+        'media/sound-effect-dog-deep-barking-01-367309.mp3');
     
     } else if (type === 'cat') {
       currentPet = new Pet('Cat', 'You have petted the cat! 🐱', 'https://images.pexels.com/photos/3030635/pexels-photo-3030635.jpeg', 
@@ -50,7 +50,7 @@ class Pet {
           'media/parrots-88486.mp3');
     }
   
-    document.getElementById('petImage').src = currentPet.image; 
+    document.getElementById('petImage').src = currentPet.image; //also mapping to html
     document.getElementById('game').style.display = 'block';
     updateCounter(0);
     showFunActions(currentPet.name);
@@ -68,7 +68,7 @@ class Pet {
   // ____________________________________
   //     Function to update the counter
   // ____________________________________
-  function updateCounter(count) { 
+  function updateCounter(count) { //count is mapping, marges the counter variable with the parametrers so that it keeps playing over n over
     document.getElementById('counter').innerText = `Times played: ${count}`;
   }
   
@@ -91,7 +91,12 @@ class Pet {
         actions = ['Squak', 'Mimic words', 'Flies'];
       }
   
-    const actionBox = document.getElementById('actions'); 
+  //function showFunActions(petName) {
+    //const actions = petName === 'Dog'
+     // ? ['Bark', 'Wag Tail', 'Fetch Ball']
+      //: ['Meow', 'Purr', 'Chase Mouse']
+  
+    const actionBox = document.getElementById('actions');  //to load properties
     actionBox.innerHTML = '<strong>Fun Actions:</strong><br>';
   
     // Loop through actions and show them
